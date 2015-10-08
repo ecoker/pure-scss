e5065bc57a6a0ed98612e16c0f4fbe92656539be
Ehren Coker <mrcoker@gmail.com>
2015-10-08 10:55:38 -0500

    Remove demo styles from core.scss

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,6 @@
 	<meta charset="UTF-8">
 	<title>Pure SCSS <?php echo (isset($page) ? ' | ' . $page : ''); ?></title>
 	<link rel="stylesheet" href="styles/core.css">
+	<link rel="stylesheet" href="styles/demo.css">
 	<link rel="stylesheet" href="styles/responsive-grid.css">
 	<link rel="stylesheet" href="styles/prism.css">
